359473abf7fc8324dfadb7f6a15f73b16a36b426
Marcus Lopes <marcus.p.lopes@gmail.com>
2016-01-26 15:08:14 -0800

    Fixed heroku path

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,2 +1,7 @@
 GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc'
-GrapeSwaggerRails.options.app_url  = "http://localhost:3000"
+
+if Rails.env.production?
+	GrapeSwaggerRails.options.app_url  = "http://tidy-api-test.herokuapp.com"
+else 
+	GrapeSwaggerRails.options.app_url  = "http://localhost:3000"
+end
