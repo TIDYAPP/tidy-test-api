efa4e5afe33bd90229a51462741e9dc85657c5dc
Marcus Lopes <marcus.p.lopes@gmail.com>
2019-07-04 12:20:42 -0700

    Cors for Heroku - Config.ru

--- config/application.rb
+++ config/application.rb
@@ -16,15 +16,15 @@ module TidyApiTest
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
-      allow do
-        origins '*'
-        resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-          :max_age => 0,
-          :expose => :location
-      end
-    end
+    #config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+    #  allow do
+    #    origins '*'
+    #    resource '*',
+    #      :headers => :any,
+    #      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+    #      :max_age => 0,
+    #      :expose => :location
+    #  end
+    #end
   end
 end
